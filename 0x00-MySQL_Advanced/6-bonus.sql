4b92e66efbd1b7c8b4af080d110ccec4d01200f4
motirase <117908555+motirase@users.noreply.github.com>
2023-10-13 15:03:03 +0300

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,19 +1,25 @@
--- SQL script that creates a stored procedure AddBonus that adds a new correction for a student
--- Procedure AddBonus is taking 3 inputs (in this order):
--- user_id, a users.id value (you can assume user_id is linked to an existing users)
---cproject_name, a new or already exists projects - if no projects.name found in the table, you should create it
--- score, the score value for the correction
-DELIMITER $$ ;
-CREATE PROCEDURE AddBonus(
-	IN user_id INTEGER,
-	IN project_name VARCHAR(255),
-	IN score INTEGER
-)
+-- Creates a stored procedure AddBonus which adds a new
+-- correction for a student
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER //
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
-		INSERT INTO projects (name) VALUES (project_name);
-	END IF;
-	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
-END;$$
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
+
+    SELECT COUNT(id)
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
+    IF project_count = 0 THEN
+        INSERT INTO projects(name)
+            VALUES(project_name);
+    END IF;
+    SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+    INSERT INTO corrections(user_id, project_id, score)
+        VALUES (user_id, project_id, score);
+END //
 DELIMITER ;
