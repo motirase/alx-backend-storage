52e31e86f06fc71e1672b1d78d46c170001c4892
motirase <117908555+motirase@users.noreply.github.com>
2023-10-18 17:57:09 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,20 @@
 #!/usr/bin/env python3
 """
-Implements an expiring web cache and tracker
+create a web cach
 """
-from typing import Callable
-from functools import wraps
 import redis
 import requests
-redis_client = redis.Redis()
+rc = redis.Redis()
+count = 0
 
 
-def url_count(method: Callable) -> Callable:
-    """counts how many times an url is accessed"""
-    @wraps(method)
-    def wrapper(*args, **kwargs):
-        url = args[0]
-        redis_client.incr(f"count:{url}")
-        cached = redis_client.get(f'{url}')
-        if cached:
-            return cached.decode('utf-8')
-        redis_client.setex(f'{url}, 10, {method(url)}')
-        return method(*args, **kwargs)
-    return wrapper
-
-
-@url_count
 def get_page(url: str) -> str:
-    """get a page and cache value"""
-    response = requests.get(url)
-    return response.text
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
 
 
 if __name__ == "__main__":
